1f608968e48f4e3c7ce06e077b30ab8a85eaf8f0
hirofumi omi <omihirofumi@gmail.com>
2023-06-28 21:48:05 +0900

    Fan-In Fan-Out

---
+++ chap04/fanout_test.go
@@ -0,0 +1,34 @@
+package chap04
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestSplit(t *testing.T) {
+	source := make(chan int)
+	dests := Split(source, 5)
+
+	go func() {
+		for i := 1; i <= 10; i++ {
+			source <- i
+		}
+
+		close(source)
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(len(dests))
+
+	for i, ch := range dests {
+		go func(i int, d <-chan int) {
+			defer wg.Done()
+
+			for val := range d {
+				fmt.Printf("#%d got %d\n", i, val)
+			}
+		}(i, ch)
+	}
+	wg.Wait()
+}
