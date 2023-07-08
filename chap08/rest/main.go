1403765118e0f2bbc1b61a93b7d2ca16293549d1
hirofumi omi <omihirofumi@gmail.com>
2023-07-08 17:50:59 +0900

    add POST usage

--- chap08/rest/main.go
+++ chap08/rest/main.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
-func main() {
+func Get() {
 	resp, err := http.Get("http://example.com")
 	if err != nil {
 		panic(err)
@@ -18,4 +19,26 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(string(body))
+}
+
+func Post() {
+	const json = `{ "name": "Hiro", "age": 27}`
+	in := strings.NewReader(json)
+
+	// not found
+	resp, err := http.Post("http://example.com/upload", "text/json", in)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	message, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf(string(message))
+}
+
+func main() {
+	Post()
 }
